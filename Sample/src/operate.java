aba9ca6af9661426ca798e6dd995d486b41ad528
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-02-06 16:33:43 +0800

    Add files via upload

---
+++ Sample/src/operate.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class operate {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        input();
+        handle();
+        output();
+    }
+
+    public static void input()
+    {
+
+    }
+
+    public static void handle()
+    {
+
+    }
+
+    public static void output()
+    {
+
+    }
+}
+
