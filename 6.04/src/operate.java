c0204fce2245dc6922674805ba09db00312a9fa5
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-02-09 22:10:58 +0800

    Add files via upload

---
+++ 6.04/src/operate.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class operate {
+    static Scanner sc = new Scanner(System.in);
+    static int[][] arr;
+
+    public static void main(String[] args)
+    {
+        input();
+        handle();
+        output();
+    }
+
+    public static void input()
+    {
+        System.out.println("Program:Please input the lines number of number array.");
+        System.out.print("User:");
+        int line_num = sc.nextInt();
+        System.out.println("Program:Please input the number array.");
+        System.out.print("User:");
+        for(int i = 1; i <= line_num; i++)
+        {
+            arr[i] = array_input();
+        }
+    }
+
+    public static int[] array_input()//这是一个不需要先给定数组长度就能直接输入数组的方法
+    {
+        String Str = sc.nextLine();//输入字符串Str
+        Str = Str.trim();//去掉字符串Str首尾的空格
+        //System.out.println(Str);
+        String[] split_array = Str.split("\\s+");//根据空白字符分割字符串Str
+        /*//测试字符串分割效果
+        System.out.println("Test output started");
+        for(String s:split_array)
+        {
+            System.out.print(s+" ");
+        }
+        System.out.println();
+        System.out.println(split_array.length);//输出字符串分割后子字符串的数量
+        System.out.println("Test output end");
+        //*/
+        int[] array = new int[split_array.length];//定义数组
+        for(int i = 0; i < split_array.length; i++)
+        {
+            array[i] = Integer.parseInt(split_array[i]);//将分割得到的子字符串中的字符类文本转化为数字类文本赋值到数组中
+        }
+        /*//测试输出数组
+        System.out.println("Test output started");
+        for(int x:array)
+        {
+            System.out.print(x+" ");
+        }
+        System.out.println("\nTest output end");
+        //*/
+        return array;//返回数组
+    }
+
+    public static void handle()
+    {
+
+    }
+
+    public static void output()
+    {
+
+    }
+}
