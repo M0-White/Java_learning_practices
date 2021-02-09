0dbf54aabffca8b8762a2f53f86e292b560d8514
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-02-10 00:04:13 +0800

    update

--- 2.02/src/operate.java
+++ 2.02/src/operate.java
@@ -1,5 +1,6 @@
 public class operate {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("7+3="+(7+3));
         System.out.println("7-3="+(7-3));
         System.out.println("7*3="+(7*3));
