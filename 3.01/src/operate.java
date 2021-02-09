adc888294c669873a6abf8f6231ec3d28dfd2dfe
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-02-09 22:48:05 +0800

    Update operate.java

--- 3.01/src/operate.java
+++ 3.01/src/operate.java
@@ -1,7 +1,9 @@
-public class operate {
-    public static void main(String[] args) {
-        int a=2;
-        int b=8;
-        System.out.println("a+b="+(a+b));
+public class operate 
+{
+    public static void main(String[] args) 
+    {
+        int a = 2;
+        int b = 8;
+        System.out.println("a+b=" + (a+b));
     }
 }
