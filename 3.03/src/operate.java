f829293b7a690ee4c10f6425ae326a287cc3dda1
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-02-12 15:32:22 +0800

    Update operate.java

--- 3.03/src/operate.java
+++ 3.03/src/operate.java
@@ -2,15 +2,15 @@ public class operate
 {
     public static void main(String[] args)
     {
-        byte b=8;
-        short s=14;
-        int i=4564;
-        long l=15949498;
+        byte b = 8;
+        short s = 14;
+        int i = 4564;
+        long l = 15949498;
 
-        int tb=b;
-        int ts=s;
-        int ti=i;
-        int tl=(int)l;
+        int tb = b;
+        int ts = s;
+        int ti = i;
+        int tl = (int)l;
 
         System.out.println(tb);
         System.out.println(ts);
