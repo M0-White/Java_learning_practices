77a38e2922ee4ccc563ea266fc5cbc4bfced1e57
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-02-15 12:00:35 +0800

    Add files via upload

---
+++ 7.03/src/operate.java
@@ -0,0 +1,9 @@
+public class operate {
+    public static void main(String[] args) {
+        int length = args.length;
+
+        for (int i = 1; i <= length; i++) {
+            System.out.println(i);
+        }
+    }
+}
