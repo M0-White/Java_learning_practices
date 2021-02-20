db5a9b310bc90357e0b6950001ed6a4adb8f60a1
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-02-20 23:40:31 +0800

    Update operate.java

--- 3.02/src/operate.java
+++ 3.02/src/operate.java
@@ -1,9 +1,9 @@
 public class operate //java类operate
 {
-    static int age=19; //声明成员变量age
+    static int age = 19; //声明成员变量age
     public static void main(String[] args) //主方法
     {
-        String name="Tolia";//声明局部变量name
-        System.out.println(name+"的年龄是"+age);//输出name,age
+        String name = "Tolia"; //声明局部变量name
+        System.out.println(name + "的年龄是" + age); //输出name,age
     }
 }
